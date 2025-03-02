15b50f720d80484f49adb2d27f18c00225cba72c
Saumil-Shah-itp <123384549+Saumil-Shah-itp@users.noreply.github.com>
2025-03-02 22:10:28 +0530

    Update asg-backend.sh

--- asg-backend.sh
+++ asg-backend.sh
@@ -3,4 +3,4 @@ yum update -y
 yum install -y python3 python3-pip git
 pip3 install flask flask-cors mysql-connector-python requests pymsql
 cd /home/ec2-user/flaskapp/backend/
-python3 /home/ec2-user/flaskapp/backend/app.py
+python3 /home/ec2-user/flaskapp/backend/backend.py
