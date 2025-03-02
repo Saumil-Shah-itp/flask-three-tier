585983ba3b23fd6206db331c78edf6abe58569bd
Saumil-Shah-itp <123384549+Saumil-Shah-itp@users.noreply.github.com>
2025-03-02 22:07:47 +0530

    Add files via upload

---
+++ asg-backend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+yum update -y
+yum install -y python3 python3-pip git
+pip3 install flask flask-cors mysql-connector-python requests pymsql
+cd /home/ec2-user/flaskapp/backend/
+python3 /home/ec2-user/flaskapp/backend/app.py
