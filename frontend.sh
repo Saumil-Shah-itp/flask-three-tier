9c41d06f622e9c498457b41b6d764eafde144615
Saumil-Shah-itp <123384549+Saumil-Shah-itp@users.noreply.github.com>
2025-03-02 22:09:17 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -4,4 +4,4 @@ yum install -y python3 python3-pip git
 git clone https://github.com/Saumil-Shah-itp/flask-three-tier.git /home/ec2-user/flaskapp
 cd /home/ec2-user/flaskapp/frontend
 pip3 install flask flask-cors mysql-connector-python requests pymysql
-sudo python3 /home/ec2-user/flaskapp/frontend/app.py
+sudo python3 /home/ec2-user/flaskapp/frontend/frontend.py
