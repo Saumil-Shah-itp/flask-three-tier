f15a0f64fe7b8a9163ed39bcebd1fbf94f3b4b86
Saumil-Shah-itp <123384549+Saumil-Shah-itp@users.noreply.github.com>
2025-03-02 22:09:56 +0530

    Update backend.sh

--- backend.sh
+++ backend.sh
@@ -4,4 +4,4 @@ yum install -y python3 python3-pip git
 git clone https://github.com/Saumil-Shah-itp/flask-three-tier.git /home/ec2-user/flaskapp
 cd /home/ec2-user/flaskapp/backend
 pip3 install flask flask-cors mysql-connector-python requests pymysql
-sudo python3 /home/ec2-user/flaskapp/backend/app.py 
+sudo python3 /home/ec2-user/flaskapp/backend/backend.py 
