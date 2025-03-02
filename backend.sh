74fd8fede59ea7bda80af94e2ad745bd23984199
Saumil-Shah-itp <123384549+Saumil-Shah-itp@users.noreply.github.com>
2025-03-02 21:50:04 +0530

    Add files via upload

---
+++ backend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum update -y
+yum install -y python3 python3-pip git
+git clone https://github.com/Saumil-Shah-itp/flask-three-tier.git /home/ec2-user/flaskapp
+cd /home/ec2-user/flaskapp/backend
+pip3 install flask flask-cors mysql-connector-python requests pymysql
+sudo python3 /home/ec2-user/flaskapp/backend/app.py 
