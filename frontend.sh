c8c46f52f88e5c6792d5fa513b20708d0bef239f
Saumil-Shah-itp <123384549+Saumil-Shah-itp@users.noreply.github.com>
2025-03-08 15:01:12 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -5,4 +5,5 @@ git clone https://github.com/Saumil-Shah-itp/flask-three-tier.git /home/ec2-user
 cd /home/ec2-user/flaskapp/frontend
 sudo pip3 install flask flask-cors mysql-connector-python requests pymysql
 sudo pip3 install --upgrade urllib3==1.26.16
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 sudo python3 /home/ec2-user/flaskapp/frontend/frontend.py
